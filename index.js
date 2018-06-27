e48fc5f4cfcf94ab93e4e61a65a91aa92f409343
Marco Dagrada <marco.dagrada@telnext.com>
2018-06-27 11:32:21 +0200

    Bugfix

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 
 app.get('/search', (request, response) => {
   let tk = '';
-  if(request.query && request.query.searchtoken){
+  if(request.query !== undefined && request.query.searchtoken !== undefined){
     tk = request.query.searchtoken;
   }
   const queryString = tk.replace(/[\\$'"]/g, "\\$&");
